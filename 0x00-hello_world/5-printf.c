2fbb20ae41aea13f35686d9b38eefa47f226e37e
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-10 05:26:18 -0700

    printf function and correct betty syle

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * This program print with the standard
+ * output function printf
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
