609996cee422c3a3bfd3a9bd686bfc8a382cc0ae
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-10 05:02:53 -0700

    betty style corrected

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *  * main - Entry point
  *   *using puts to print
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+	return (0);
 }
