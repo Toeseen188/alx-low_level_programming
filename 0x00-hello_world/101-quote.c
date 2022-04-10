7ddc66a162648cd2be8b96163fb13b1dd9ddea2f
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-10 15:47:45 -0700

    betty code style fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /*
- * main- entry point for a c program
- * Return: return 1 to indicate the error
+ *main- entry point for a c program
+ *Return: return 1 to indicate the error
  *
  */
 int main(void)
 {
 
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
