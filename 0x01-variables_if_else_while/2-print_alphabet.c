40d2fe51f73ca92939882a250c69506178872c1c
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-11 08:42:04 -0700

    printing alphabets with putchar func

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - contains all my compilable codes
+ * Return: 
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return(0);
+}
