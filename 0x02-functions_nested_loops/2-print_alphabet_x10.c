fadf2eff07033ac993c76e301590736eb251e5ac
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-12 09:42:29 -0700

    printing a to z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this function prints 'a' to 'z' 10 times
+ *@c: char c is local variable to the function
+ * Return: _putchar returns a single character to the sys call
+ * Description: this is cool function program
+*/
+
+int _putchar(char c);
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
