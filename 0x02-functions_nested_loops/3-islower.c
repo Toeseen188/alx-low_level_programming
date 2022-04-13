0c8b73cf1d1d4fc99a429783478512b52ad28fa3
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-13 13:59:34 -0700

    islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - returns the args as lowercase if true
+ * Return: returns an interger value, true = 1
+ * @c : parameter for islower
+*/
+
+int _islower(int c)
+{
+
+	return (c >= 'a' && c <= 'z');
+}
