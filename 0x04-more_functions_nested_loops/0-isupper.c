4da85c577bcf0ea68eecd1d53aa3bb60b8b9890d
Hammed Tosin <hammedtosin188@gmail.com>
2022-04-15 15:20:57 -0700

    betty taken into consideration

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include "main.h"
-
+/**
+ * _isupper - function to return 1 if uppercase
+ * Return: returns some parameters
+ * @c : argument
+*/
 int _isupper(int c)
 {
-	return(c <= 'A' && c <= 'Z');
+	return (c <= 'A' && c <= 'Z');
 }
