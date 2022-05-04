b39efe0fda7964ff711fc4616582d11c41843233
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-04 01:07:29 -0700

    betty considered

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns 0 success
+ */
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	argc = 0;
 
-	printf("%s\n",argv[argc]);
-	
+	printf("%s\n", argv[argc]);
+
 	return (0);
 
 }
