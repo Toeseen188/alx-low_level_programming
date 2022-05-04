6cb21f5143a3e2a4e87a8fcca34bf1a168593fce
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-04 03:01:33 -0700

    print argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument counts
+ * @argv: arguments vector
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+
+{
+	int i = 0;
+
+	argv[i] = argv[i];
+	printf("%d\n", argc);
+
+	return (0);
+}
