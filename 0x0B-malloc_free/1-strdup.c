3ece3a28244594e480b0133ad80414a3e6630192
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-05 16:21:20 -0700

    hopes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,12 +14,13 @@ char *_strdup(char *str)
 	int i;
 	int a = _strlen(str) + 1;
 
-	s = malloc(a * sizeof(char));
+	s = malloc(a);
 	if (str == NULL || s == NULL)
 	return (NULL);
 	else
 	for (i = 0; i < a; i++)
 	s[i] = str[i];
+	s[i] = '\0';
 	return (s);
 	free(s);
 
