6ab781e6a86f9f8be77a8d3f505fb80cce24e095
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-05 17:04:56 -0700

    fixing

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,27 +2,43 @@
 #include <stdlib.h>
 /**
  * _strdup - allocate and duplicate string
- * @str: character parameter
+ *@c: param
  * Return: returns NULL if str || s == Null, otherwise return s
  */
-
-
+/* function prototype*/
+int _strlen(char *c);
 
 char *_strdup(char *str)
 {
 	char *s;
 	int i;
+
 	int a = _strlen(str) + 1;
 
-	s = malloc(a);
+	s = malloc(a * sizeof(char));
 	if (str == NULL || s == NULL)
-	return (NULL);
+	return ('\0');
 	else
 	for (i = 0; i < a; i++)
 	s[i] = str[i];
-	s[i] = '\0';
 	return (s);
 	free(s);
 
 
+}
+
+/**
+ * _strlen - length of a string
+ * @c: param
+ * Return: i
+ */
+
+int _strlen(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+	i++;
+	return (i);
 }
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,6 +2,5 @@
 #define MAIN_H
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-int _strlen(char *s);
 char *_strdup(char *str);
 #endif
