8de0a19e01e2b6bf7cd2a3fc1957fc2b0cd0587f
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-19 06:45:25 -0700

    tough

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - function with variadic param
+ * @n: unsigned int var
+ * @separator: character pointer to store char
+ * Return: nothing
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i = 0;
+
+	va_start(ap, n);
+	if (separator == NULL)
+	{
+	};
+	while (i < n)
+	{
+	printf("%d", va_arg(ap, int));
+	while (i < (n - 1))
+	{
+	printf("%s ", separator);
+	break;
+	}
+	i++;
+	}
+	va_end(ap);
+	printf("\n");
+}
