3780d4e5fae0c17f18a3d763673fc9da968e1a72
Hammed Tosin <hammedtosin188@gmail.com>
2022-05-25 09:56:34 -0700

    attribute constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_first - function to print string before main function
+ * Return: void
+*/
+
+void print_first(void)
+{
+	char *str = "You're beat! and yet, you must allow,";
+	char *str2 = "I bore my house upon my back!\n";
+
+	printf("%s\n%s", str, str2);
+}
