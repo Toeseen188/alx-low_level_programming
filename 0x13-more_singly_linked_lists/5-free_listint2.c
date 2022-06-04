1200dfb2fccb0dce4e1700bf88f009cd3172853b
Hammed Tosin <hammedtosin188@gmail.com>
2022-06-03 20:16:31 -0700

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,35 @@
 #include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint2 - function to free listint_t and make head, NULL
- * @head: param
- * Return: void
+ * free_listint - free a struct linked list
+ * @head: first element
+ * Return: frees a list_t list
+*/
+void free_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+	return;
+	}
+	free_listint(head->next);
+	free(head);
+}
+
+/**
+ * free_listint2 - free a struct linked list
+ * @head: first element
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *new, *temp;
+	listint_t *new;
 
 	if (head == NULL)
+	{
 	return;
+	}
 
 	new = *head;
-	if (new != NULL)
-	{
-	temp = new;
-	new = new->next;
-	free(temp);
-	}
 	*head = NULL;
+	free_listint(new);
 }
