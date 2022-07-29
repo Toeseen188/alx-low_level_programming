ab31800001f4b4b6b1a15cfa6fee0dc8071dd3af
Hammed Tosin <hammedtosin188@gmail.com>
2022-07-29 14:16:37 -0700

    betty fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - create a new has table
+ * @size: the size of the table to be created
+ * Return: returns the pointer to the new table
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -9,22 +13,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
-		return NULL;
+		return (NULL);
 
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (new_table->array == NULL)
-	{	
+	{
 	free(new_table->array);
 	free(new_table);
-	return NULL;
+	return (NULL);
 	}
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
-	
-	return new_table;
+
+	return (new_table);
 
 
 }
