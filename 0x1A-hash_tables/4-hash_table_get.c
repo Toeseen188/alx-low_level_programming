4b09476c2bf472de2b49c8241f3969614752cde8
Hammed Tosin <hammedtosin188@gmail.com>
2022-09-23 09:32:48 -0700

    retrieve the value of a hashnode from the hash_table using the key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get- retrieve a value using the key
+ * @ht: the table to check to retrieve from
+ * @key: the key to look for
+ * Return: return the value of the key retrieved
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+	char *value = NULL;
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	for (i = index; ht->array[i]; i++)
+	{
+	if (strcmp(key, ht->array[i]->key) == 0)
+		value = ht->array[i]->value;
+	}
+	return (value);
+}
