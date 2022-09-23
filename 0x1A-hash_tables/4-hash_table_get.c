7bbb933f926df50731e20f725c9ec56db6ae0711
Hammed Tosin <hammedtosin188@gmail.com>
2022-09-23 09:45:06 -0700

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index, i;
 	char *value = NULL;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
 	for (i = index; ht->array[i]; i++)
 	{
